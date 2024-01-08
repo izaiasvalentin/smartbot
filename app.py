2710312d15f059fee40f921e9c44c2fada40f9a3
izaiasvalentin <155989428+izaiasvalentin@users.noreply.github.com>
2024-01-08 17:29:20 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import base64
+
+app = Flask(__name__)
+
+@app.get('/')
+def index():
+    return 'smartapi'
+
+@app.get('/<palavra>')
+def cripto_mensagem(palavra):
+    chave = b'c21hcnRib3Q2@g5f'
+    iv = b'RP2otAWFtvZY0gT1'
+    cipher = Cipher(algorithms.AES(chave), modes.CFB(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    mensagem_cifrada = encryptor.update(palavra.encode()) + encryptor.finalize()
+    return base64.b64encode(mensagem_cifrada).decode()   
+
+if __name__ == '__main__':
+    app.run()
